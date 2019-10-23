66c06b41b35dd76ac0a58f68f12682bec507d00c
Dominik Kolasiński <dominik.kolasinski@10clouds.com>
2019-10-23 19:47:33 +0200

    setup jest/enzyme (#2)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>fomr-demo</h1>
       </header>
     </div>
   );
