9b97dbeb4d034e008d312de0fa353bf1052b40e7
Dominik Kolasiński <dominik.kolasinski@10clouds.com>
2019-10-28 12:26:13 +0100

    app test refactor

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -3,8 +3,15 @@ import { shallow } from "enzyme";
 import App from "../src/App";
 
 describe("<App /> rendering", () => {
-  it("should render App", () => {
+  it("should render App with router", () => {
     let wrapper = shallow(<App />);
-    expect(wrapper.find(".app").length).toBe(1);
+
+    expect(wrapper.find("div").hasClass("app")).toBe(true);
+    expect(wrapper.find("header").hasClass("header")).toBe(true);
+    expect(wrapper.find("h1").hasClass("title")).toBe(true);
+    expect(wrapper.find("h1").text()).toEqual("Form Demo");
+    expect(wrapper.find("BrowserRouter").length).toBe(1);
+    expect(wrapper.find("Switch").length).toBe(1);
+    expect(wrapper.find("Route").length).toBe(3);
   });
 });
